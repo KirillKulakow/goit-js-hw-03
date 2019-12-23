2c3759b947323ce27e03ea93124e93faea4efa49
Kirill <kirillkulakow31@gmail.com>
2019-12-23 22:50:13 +0200

    add module 3

---
+++ task-1.js
@@ -0,0 +1,17 @@
+'use strict';
+/*
+Task 1
+*/
+
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+  };
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+for (const key of Object.keys(user)) {
+    console.log(key, ':', user[key]);
+}
